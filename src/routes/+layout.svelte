733b70066cbeee7165ac2729dbdaa24f79b0a5c1
Kye Russell <me@kye.id.au>
2023-02-20 08:31:37 +0800

    more stuff.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,9 @@
 <div
 	class="grid h-full grid-rows-[auto,1fr,auto] divide-y-4 divide-neutral-700 font-serif text-neutral-700"
 >
-	<header class="px-4 py-2 text-center md:py-4 md:px-20">
+	<header class="bg-amber-50 px-4 py-2 text-center md:py-4 md:px-20">
 		<h1 class="flex-grow font-['Roboto_Slab'] text-5xl font-bold md:text-8xl">
-			choice<span class="text-amber-300">.</span>tools
+			choice<span class="text-amber-400">.</span>tools
 		</h1>
 	</header>
 	<main class="px-4 py-2 md:px-20 md:py-4">
